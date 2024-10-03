0f85417bb07efa3ebc41d78c8f3520c60eeaef15
fntita81 <fntita81@gmail.com>
2024-10-03 21:47:47 +0100

    initial commit

---
+++ Pages/login.js
@@ -0,0 +1,18 @@
+export class LoginPage {
+  constructor(page) {
+    this.page = page;
+
+    this.username_textbox = page.getByLabel('Username');
+    this.password_textbox = page.getByLabel('Password');
+    this.login_button = page.getByRole('button', { name: 'Login' });
+  }
+  async login(username, password) {
+    await this.username_textbox.fill(username);
+    await this.password_textbox.fill(password);
+    await this.login_button.click();
+    }
+    async gotoLoginPage() {
+        await this.page.goto('https://the-internet.herokuapp.com/login');
+
+    }
+}
---
+++ tests/login.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test'
+import{LoginPage} from '../Pages/login'
+test('test', async ({ page }) => {
+
+  const Login = new LoginPage(page)
+  
+  await Login.gotoLoginPage()
+  await Login.login('tomsmith','SuperSecretPassword!')
+  
+    // await page.goto('https://the-internet.herokuapp.com/login');
+    // await page.getByLabel('Username').click();
+    // await page.getByLabel('Username').fill('tomsmith');
+    // await page.getByLabel('Password').click();
+    // await page.getByLabel('Password').fill('SuperSecretPassword!');
+    // await page.getByRole('button', { name: ' Login' }).click();
+    // await page.getByRole('heading', { name: 'Secure Area', exact: true }).click();
+    // await page.getByRole('link', { name: 'Logout' }).click();
+    // await page.get
+  });
